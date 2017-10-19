8dc7078e5e2be2457bd41f43172d7c9a9ccde6e7
Chang Yanwei <changyanwei@gmail.com>
2017-10-19 12:35:27 +0800

    [yanwei] initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,38 @@
+import webpack from 'webpack'
+import path from 'path'
+
+export default {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    noInfo: false,
+    contentBase: './src'
+  },
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    './src/index'
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+}
