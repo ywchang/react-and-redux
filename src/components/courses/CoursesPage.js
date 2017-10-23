7cc4b1e00744ed34fe8e63a7e70c0d828071fd54
Chang Yanwei <changyanwei@gmail.com>
2017-10-23 18:01:51 +0800

    [yanwei] set up course form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,32 @@
 import * as React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: {title: ""}
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
+        <input type="submit" value="Save" onClick={this.onClickSave}/>
       </div>
     );
   }
